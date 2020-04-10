3d67a566706537b00a7db1d93e3be8f9ebf8deae
Joseph Surin <joseph.surin@gmail.com>
2020-04-10 10:37:18 +1000

    add task 2

--- src/tasks/tasks.js
+++ src/tasks/tasks.js
@@ -1,4 +1,4 @@
-const TASK_1 = {
+const TASK_0 = {
     id: 0,
     name: 'Add The Inputs',
     desc: 'Read two input values, add them together, and output the result as an integer.',
@@ -6,6 +6,15 @@ const TASK_1 = {
     outputs: [[2], [48], [-5]]
 }
 
+const TASK_1 = {
+    id: 1,
+    name: 'Multiply The Inputs',
+    desc: 'Read three input values, multiply them together, and output the result as an integer.',
+    inputs: [[1,2,3], [-1,-2,-3], [0,1,2], [11,22,33]],
+    outputs: [[6], [-6], [0], [7986]]
+}
+
 module.exports = [
+    TASK_0,
     TASK_1
 ]
